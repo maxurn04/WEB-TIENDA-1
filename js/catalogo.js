0bdcb9423d321807b8c272dc492ec0c86f77a842
maxurn04 <maurrutnx@gmail.com>
2026-09-03 01:29:03 -0400

    correcciones en el css y estilos

--- js/catalogo.js
+++ js/catalogo.js
@@ -4,23 +4,24 @@ const catalogo = [
     { id: 1, nombre: "PlayStation 2", precio: 60000, img: "../img/ps2.jpg", categoria: "Consolas"},
     { id: 2, nombre: "Zelda 64", precio: 30000, img: "../img/zelda64.webp", categoria: "Juegos"},
     { id: 3, nombre: "Bolso Cartier", precio: 50000, img: "../img/bolsoEjemplo.webp", categoria: "Accesorios"},
-    { id: 4, nombre: "PlayStation 3", precio: 85000, img: "../img/ps3.jpg", categoria: "Consolas"}
+    { id: 4, nombre: "PlayStation 3", precio: 85000, img: "../img/ps3.jpg", categoria: "Consolas"},
+    { id: 5, nombre: "Zeebo", precio: 35000, img: "../img/zeebo.jpeg", categoria: "Consolas"},
+    { id: 6, nombre: "GameCube", precio: 40000, img: "../img/gamecub.jpg", categoria: "Consolas"}
 ];
 
 
 function renderizarProductos() {
-    var contenedor = document.getElementById('grilla-productos');
-    if(!contenedor) return;
+    var conte = document.getElementById('grilla-productos');
+    if(!conte) return;
 
-    catalogo.forEach(prod => {
-        contenedor.innerHTML += 
+    catalogo.forEach(prod => {conte.innerHTML += 
             `<div class="col-12 col-sm-6 col-lg-3 mb-4">
                 <div class="caja-peque">
                     <div>
                         <a href="detalleprod.html?id=${prod.id}">
-                            <img src="${prod.img}" alt="${prod.nombre}" class="img-fluid" style="max-width: 100%; height: 150px; border-radius: 10px; object-fit: cover;">
+                            <img src="${prod.img}" alt="${prod.nombre}" class="img-fluid imagen-productos">
                         </a>
-                        <p class="fuente-palabras" style="padding-top: 7px; font-size: 12px; text-transform: uppercase;">
+                        <p class="fuente-palabras" style="padding-top: 6px; font-size: 14px;">
                             ${prod.nombre}
                         </p>
                     </div>
